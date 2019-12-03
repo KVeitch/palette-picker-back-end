a2124b011e71048515afbdcd2d3f5a048e5a445f
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-02 19:25:36 -0700

    Improve migration tables

--- db/migrations/20191202153352_initial.js
+++ db/migrations/20191202153352_initial.js
@@ -3,15 +3,15 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('users', table => {
       table.increments('id').primary();
-      table.string('user_name');
-      table.string('password');
+      table.text('user_name');
+      table.text('password');
 
       table.timestamps(true, true);
     }),
 
     knex.schema.createTable('projects', table => {
       table.increments('id').primary();      
-      table.string('project_name');
+      table.text('project_name');
       table.unique('project_name');
       table.integer('user_id').references('users.id')
 
@@ -20,13 +20,13 @@ exports.up = function(knex) {
 
     knex.schema.createTable('palettes', table => {
       table.increments('id').primary();
-      table.string('palette_name');
+      table.text('palette_name');
       table.integer('project_id').references('projects.id')
-      table.string('color0');
-      table.string('color1');
-      table.string('color2');
-      table.string('color3');
-      table.string('color4');
+      table.string('color0',6);
+      table.string('color1',6);
+      table.string('color2',6);
+      table.string('color3',6);
+      table.string('color4',6);
       
       table.timestamps(true, true)
     })
