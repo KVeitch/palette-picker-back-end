ff97b2ba12c8cf568689d7b05ab227ffeed1535a
ericwm76 <eric.meldrum@gmail.com>
2019-12-03 14:09:27 -0700

    Create .babelrc file to enable npm start and npm test scripts to run
    Co-authored by: Kirk Veitch <kirk.a.veitch@gmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
+app.locals.title = 'Palette Picker Backend'
 app.use(cors());
 app.use(express.json());
-
 
 app.get('/', (request, response) => {
   response.send('Palette picker - pick the palettes!');
--- app.test.js
+++ app.test.js
@@ -1,12 +1,16 @@
 import "@babel/polyfill";
-import request from 'supertest'
-import app from './app'
+import request from 'supertest';
+import app from './app';
 
-const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
-const database = require('knex')(configuration)
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 describe('Server', () => {
+  beforeEach(async () => {
+    await database.seed.run()
+  });
+  
   describe('init', () => {
     it('should return a 200 status', async () => {
       const res = await request(app).get('/')
