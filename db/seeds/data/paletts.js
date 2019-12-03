88d9ba181119bd70e7ac5e6644d41a32b3b7999a
ericwm76 <eric.meldrum@gmail.com>
2019-12-03 13:31:47 -0700

    Move data to more accessible folder

---
+++ db/seeds/data/paletts.js
@@ -0,0 +1,166 @@
+const palettes = [
+  {
+    "palette_name": "Colors",
+    "project_id": 1,
+    "color0": "6320ee",
+    "color1": "D5f2e3",
+    "color2": 999950,
+    "color3": "C7B446",
+    "color4": "C2B078"
+  },
+  {
+    "palette_name": "More Colors",
+    "project_id": 2,
+    "color0": "0a2934",
+    "color1": "Ebbab9",
+    "color2": "F3DA0B",
+    "color3": "EAE6CA",
+    "color4": "C6A664"
+  },
+  {
+    "palette_name": "Even More Colors",
+    "project_id": 3,
+    "color0": "0a2934",
+    "color1": "025877",
+    "color2": "FAD201",
+    "color3": "E1CC4F",
+    "color4": "E5BE01"
+  },
+  {
+    "palette_name": "My palette",
+    "project_id": 4,
+    "color0": "16a085",
+    "color1": "2ecc71",
+    "color2": "AEA04B",
+    "color3": "E6D690",
+    "color4": "CDA434"
+  },
+  {
+    "palette_name": "Bright",
+    "project_id": 1,
+    "color0": "786fa6",
+    "color1": "f19066",
+    "color2": "FFFF00",
+    "color3": "EDFF21",
+    "color4": "A98307"
+  },
+  {
+    "palette_name": "Shady",
+    "project_id": 2,
+    "color0": "D5f2e3",
+    "color1": 999950,
+    "color2": "C7B446",
+    "color3": "F5D033",
+    "color4": "E4A010"
+  },
+  {
+    "palette_name": "Joker",
+    "project_id": 1,
+    "color0": "211a1d",
+    "color1": "6320ee",
+    "color2": "D5f2e3",
+    "color3": "8075ff",
+    "color4": "8fc93a"
+  },
+  {
+    "palette_name": "BeachHouse",
+    "project_id": 2,
+    "color0": "Cc2936",
+    "color1": "0a2934",
+    "color2": "Ebbab9",
+    "color3": 388697,
+    "color4": "B5ffe1"
+  },
+  {
+    "palette_name": "Blues",
+    "project_id": 3,
+    "color0": "0385b5",
+    "color1": "0a2934",
+    "color2": "025877",
+    "color3": "008cbf",
+    "color4": "047199"
+  },
+  {
+    "palette_name": "Flat Bright",
+    "project_id": 4,
+    "color0": "f1c40f",
+    "color1": "16a085",
+    "color2": "2ecc71",
+    "color3": "16a085",
+    "color4": "f1c40f"
+  },
+  {
+    "palette_name": "Russian Flat",
+    "project_id": 1,
+    "color0": "f19066",
+    "color1": "786fa6",
+    "color2": "f19066",
+    "color3": "786fa6",
+    "color4": "574b90"
+  },
+  {
+    "palette_name": "Name this palette",
+    "project_id": 2,
+    "color0": "6320ee",
+    "color1": "D5f2e3",
+    "color2": 999950,
+    "color3": "C7B446",
+    "color4": "C2B078"
+  },
+  {
+    "palette_name": "Five best",
+    "project_id": 3,
+    "color0": "0a2934",
+    "color1": "Ebbab9",
+    "color2": "F3DA0B",
+    "color3": "EAE6CA",
+    "color4": "C6A664"
+  },
+  {
+    "palette_name": "For mark",
+    "project_id": 4,
+    "color0": "0a2934",
+    "color1": "025877",
+    "color2": "FAD201",
+    "color3": "E1CC4F",
+    "color4": "E5BE01"
+  },
+  {
+    "palette_name": "For Bill",
+    "project_id": 1,
+    "color0": "16a085",
+    "color1": "2ecc71",
+    "color2": "AEA04B",
+    "color3": "E6D690",
+    "color4": "CDA434"
+  },
+  {
+    "palette_name": "Porch",
+    "project_id": 2,
+    "color0": "786fa6",
+    "color1": "f19066",
+    "color2": "FFFF00",
+    "color3": "EDFF21",
+    "color4": "A98307"
+  },
+  {
+    "palette_name": "How’s this",
+    "project_id": 3,
+    "color0": "D5f2e3",
+    "color1": 999950,
+    "color2": "C7B446",
+    "color3": "F5D033",
+    "color4": "E4A010"
+  },
+  {
+    "palette_name": "Yabbadabba",
+    "project_id": 4,
+    "color0": "Ebbab9",
+    "color1": "F3DA0B",
+    "color2": "EAE6CA",
+    "color3": "F8F32B",
+    "color4": "DC9D00"
+  }
+]
+
+module.exports = palettes;
---
+++ db/seeds/data/users.js
@@ -0,0 +1,6 @@
+const users = [
+  { name: 'Bob', password: 'Bob', id: 1 },
+  { name: 'Susan', password: 'Susan', id: 2 }
+];
+
+module.exports = users;
