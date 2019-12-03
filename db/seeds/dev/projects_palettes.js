f0d74f4b09f1c008656a7416db3fc9c382d68cf5
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-02 19:26:57 -0700

    Add initial seeding file for DB

--- db/seeds/dev/projects_palettes.js
+++ db/seeds/dev/projects_palettes.js
@@ -1,13 +1,67 @@
+import users from './data/users';
+import projects from './data/projects';
+import palettes from './data/pallets';
+
+const createUser = (knex, user) => {
+  return knex('users')
+    .insert(
+      {
+        user_name: user.user_name,
+        password: user.password
+      },
+      'id'
+    )
+    .then(user_id => {
+      let projectPromises = [];
+      projects
+        .filter(project => project.user_id === user_id[0])
+        .forEach(project => {
+          projectPromises.push(
+            createProject(
+              knex,
+              {
+                project_name: project.project_name,
+                user_id: user_id[0]
+              },
+              'id'
+            ).then(project_id => {
+              let palettePromises = [];
+              palettes
+                .filter(palette => pallet.project_id === project_id[0])
+                .forEach(palette => {
+                  palettePromises.push(
+                    createPalette(knex, {
+                      pallet_name: palette.pallet_name,
+                      project_id: project_id[0],
+                      color0: palette.color0,
+                      color1: palette.color1,
+                      color2: palette.color2,
+                      color3: palette.color3,
+                      color4: palette.color4
+                    })
+                  );
+                });
+            })
+          );
+        });
+    });
+};
+
+const createProject = (knex, project) => knex('projects').insert(project);
+
+const createPalette = (knex, palette) => knex('palettes').insert(palette);
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('palettes')
+    .del()
+    .then(() => knex('projects').del())
+    .then(() => knex('users').del())
+    .then(() => {
+      let userPromises = [];
+      users.forEach(user => {
+        userPromises.push(createUser(knex, user));
+      });
+      return Promise.all(userPromises);
+    })
+    .catch(error => console.log(`We have a problem here! => ${error}`));
 };
