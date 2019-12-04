9de1b0299859bd0483452622eed479a897ed3fe7
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-12-04 15:15:09 -0700

    Correct migration file and add delete testing

--- app.js
+++ app.js
@@ -13,4 +13,126 @@ app.get('/', (request, response) => {
   response.send('Palette picker - pick the palettes!');
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.delete('/api/v1/projects/:id', async (request, response) => {
+  const { id } = request.params;
+ 
+  try {
+    const removedProject = await database('projects').where({ id:id }).del()
+    response.status(202).json(`Project ${id} was deleted`)
+  } catch(error) {
+    response.status(500).json(error)
+  }
+})
+
+app.post('/api/v1/projects', async (request, response) =>{
+  const project = request.body;
+  for (let requiredParameter of ['project_name', 'user_id']) {
+    if (!project[requiredParameter]) {
+      return response.status(422).json({
+        error: `Expected format: { project_name: <String>, user_id: <Integer> }. You're missing a "${requiredParameter}" property.`
+      });
+    }
+  }
+  try {
+    const id = await database('projects').insert(project,'id')
+
+    response.status(201).json({ ...project, id })
+
+  } catch(error) {
+    response.status(500).json(error)
+  }
+})
+
+app.get('/api/v1/users/?name+password', async (res, req)=>{
+
+
+})
+
+
+
+
 export default app;
